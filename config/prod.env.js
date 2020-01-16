a0f81c89f4adc4880eeb26f6f6520ebad7c5f88f
jliang117 <jliang117@users.noreply.github.com>
2020-01-16 15:32:26 -0500

    update dependencies

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,8 +1,4 @@
 module.exports = {
   NODE_ENV: '"production"',
-<<<<<<< HEAD
   ROOT_API: '"https://reddit-search-api.herokuapp.com/search"'
-=======
-  ROOT_API: '"https://redrec-234413.appspot.com/search"'
->>>>>>> e59523ce2f1a4bd9b09c20d7474a3a6c8ee3b5e4
 }
